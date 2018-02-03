a4266fe205762a401eb327072830ff3b1973b06c
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-02-03 19:24:14 -0200

    recuperando parametros/valores da url.

--- src/Categoria.js
+++ src/Categoria.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Categoria extends Component{
   render(){
     return(
-      <h1>Categoria</h1>
+      <h1>Categoria {this.props.match.params.catId}</h1>
     )
   }
 }
